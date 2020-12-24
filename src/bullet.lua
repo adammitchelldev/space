02f797a224959227d6535919704924550ddd0996
Adam Mitchell <adammitchelldev@gmail.com>
2020-12-24 15:32:59 +0000

    Quick bullet changes

--- src/bullet.lua
+++ src/bullet.lua
@@ -6,13 +6,22 @@ bullet = entity {
 		size = 4,
 		layer = fg_fx -- draw on top!
 	},
-	die_sfx = 1
+	die_sfx = 1,
+	age = 0,
+	scripts = {
+		function(self)
+			repeat
+				self.age += 1
+				yield()
+			until false
+		end
+	}
 }
 
 function bullet:new(base, ox, oy)
 	return self:spawn{
-		x = base.x + (ox or 0),
-		y = base.y + (oy or 0)
+		x = base.x + (ox or 0) + self.dx,
+		y = base.y + (oy or 0) + self.dy
 	}
 end
 
@@ -26,7 +35,9 @@ function bullet:draw()
 		y += self.dy
 	end
 	local colors = self.colors
-	for i = abs(self.dy)+1, 0, -1 do
+	local l = abs(self.dy)+1
+	local m = abs(self.dy)+2-self.age
+	for i = l, mid(0,m,l), -1 do
 		color(colors[c])
 		rectfill(x, y, x + self.width - 1, y + i - 1)
 		y -= i * step
